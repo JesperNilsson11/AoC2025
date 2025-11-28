c312e140bf9bdcf195a2265ffdebc5f70f535000
JesperNilsson11 <jespernilsson11@gmail.com>
2025-11-28 20:30:07 +0100

    Add test and remove main.rs

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -13,6 +13,7 @@ fn part2(file: String) -> i64 {
 
     return res;
 }
+
 fn main() {
     let file = file!();
     let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
@@ -20,4 +21,18 @@ fn main() {
     println!("Day {}!", nr);
     println!("{}", part1("input".to_string() + &nr.to_string() + ".txt"));
     println!("{}", part2("input".to_string() + &nr.to_string() + ".txt"));
+}
+
+#[test]
+fn test_part1() {
+    let file = file!();
+    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
+    assert_eq!(0, part1("test".to_string() + &nr.to_string() + ".txt"));
+}
+
+#[test]
+fn test_part2() {
+    let file = file!();
+    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
+    assert_eq!(0, part2("test".to_string() + &nr.to_string() + ".txt"));
 }
