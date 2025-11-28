f230079d85dde77256178aeab9184810f816aa14
JesperNilsson11 <jespernilsson11@gmail.com>
2025-11-28 20:16:55 +0100

    First commit, init project

---
+++ src/bin/day7.rs
@@ -0,0 +1,23 @@
+use std::{fs, path::Path};
+
+fn part1(file: String) -> i64 {
+    let input = fs::read_to_string(file).unwrap();
+    let mut res = 0;
+
+    return res;
+}
+
+fn part2(file: String) -> i64 {
+    let input = fs::read_to_string(file).unwrap();
+    let mut res = 0;
+
+    return res;
+}
+fn main() {
+    let file = file!();
+    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
+
+    println!("Day {}!", nr);
+    println!("{}", part1("input".to_string() + &nr.to_string() + ".txt"));
+    println!("{}", part2("input".to_string() + &nr.to_string() + ".txt"));
+}
