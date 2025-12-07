bc9dbea30d547d35588240cdc2cad84573fee93a
JesperNilsson11 <jespernilsson11@gmail.com>
2025-12-07 18:07:29 +0100

    Add day 6 and 7

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -4,12 +4,54 @@ fn part1(file: String) -> i64 {
     let input = fs::read_to_string(file).unwrap();
     let mut res = 0;
 
+    let map: Vec<Vec<&str>> = input.lines().map(|l| l.split_whitespace().collect()).collect();
+
+    for x in 0..map[0].len() {
+        let mut tmp: i64 = 0;
+        let op = match map.last().unwrap()[x] {
+            "+" => |n1: i64, n2: i64| n1 + n2,
+            "*" => {tmp = 1;
+                |n1, n2| n1 * n2},
+            _ => panic!(),
+        };
+        for y in 0..map.len()-1 {
+            tmp = op(tmp, map[y][x].parse().unwrap());
+        }
+        res += tmp;
+    }
+
     return res;
 }
 
 fn part2(file: String) -> i64 {
     let input = fs::read_to_string(file).unwrap();
     let mut res = 0;
+
+    let lines: Vec<_> = input.lines().collect();
+
+    let mut tmp = 0;
+    let mut op: fn(i64, i64) -> i64 = |_n1, _n2| panic!();
+    for x in 0..lines[0].len() {
+        if lines.last().unwrap().as_bytes()[x] == '*' as u8 {
+            res += tmp;
+            tmp = 1;
+            op = |n1, n2| n1 * n2;
+        } else if lines.last().unwrap().as_bytes()[x] == '+' as u8 {
+            op = |n1, n2| n1 + n2;
+        }
+
+        let mut num = 0;
+        for y in 0..lines.len()-1 {
+            if lines[y].as_bytes()[x] != b' ' {
+                num *= 10;
+                num += (lines[y].as_bytes()[x] - b'0') as i64;
+            }
+        }
+        if num > 0 {
+            tmp = op(tmp, num);
+        }
+    }
+    res += tmp;
 
     return res;
 }
@@ -27,12 +69,12 @@ fn main() {
 fn test_part1() {
     let file = file!();
     let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
-    assert_eq!(0, part1("test".to_string() + &nr.to_string() + ".txt"));
+    assert_eq!(4277556, part1("test".to_string() + &nr.to_string() + ".txt"));
 }
 
 #[test]
 fn test_part2() {
     let file = file!();
     let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
-    assert_eq!(0, part2("test".to_string() + &nr.to_string() + ".txt"));
+    assert_eq!(3263827, part2("test".to_string() + &nr.to_string() + ".txt"));
 }
--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,8 +1,91 @@
-use std::{fs, path::Path};
+use std::{collections::{HashMap, HashSet}, fs, path::Path};
 
 fn part1(file: String) -> i64 {
     let input = fs::read_to_string(file).unwrap();
     let mut res = 0;
+
+    let map: Vec<Vec<u8>> = input.lines().map(|l| l.bytes().collect()).collect();
+
+    let mut pos = (0, 0);
+    'outer: for (y, r) in map.iter().enumerate() {
+        for (x, &c) in r.iter().enumerate() {
+            if c == b'S' {
+                pos = (x as i32, y as i32);
+                break 'outer;
+            }
+        }
+    }
+
+    let mut beams = HashSet::new();
+    beams.insert(pos);
+
+    while beams.len() > 0 {
+        let mut new_beams = HashSet::new();
+
+        for b in beams.iter() {
+            let x = b.0;
+            let mut y = b.1;
+            y += 1;
+            if y >= map.len() as i32 {
+                continue;
+            }
+            if map[y as usize][x as usize] == b'^' {
+                let x1 = x - 1;
+                let x2 = x + 1;
+                res += 1;
+
+                if x1 >= 0 && x1 < map[0].len() as i32 {
+                    new_beams.insert((x1, y));
+                }
+                if x2 >= 0 && x2 < map[0].len() as i32 {
+                    new_beams.insert((x2, y));
+                }
+            } else {
+                new_beams.insert((x, y));
+            }
+        }
+
+        beams = new_beams;
+    }
+
+    return res;
+}
+
+fn dfs(x: i64, yy: i64, map: &Vec<Vec<u8>>, memo: &mut HashMap<(i64, i64), i64>) -> i64 {
+    let mut res = 0;
+
+    let mut y = yy;
+    if memo.contains_key(&(x, y)) {
+        return *memo.get(&(x,y)).unwrap();
+    }
+
+    y += 1;
+    if y >= map.len() as i64 {
+        return 1;
+    }
+
+    if map[y as usize][x as usize] == b'.' {
+        while y + 1 < map.len() as i64 && map[(y + 1) as usize][x as usize] == b'.' {
+            y += 1;
+        }
+        y += 1;
+    }
+
+    if y == map.len() as i64 {
+        return 1;
+    }
+
+    let x1 = x - 1;
+    let x2 = x + 1;
+    assert!(x1 >= 0);
+    assert!(x2 <= map[0].len() as i64);
+
+    res += dfs(x1, y, map, memo);
+    res += dfs(x2, y, map, memo);
+
+    if let Some(_) = memo.insert((x, yy), res) {
+        panic!();
+    }
 
     return res;
 }
@@ -11,12 +94,29 @@ fn part2(file: String) -> i64 {
     let input = fs::read_to_string(file).unwrap();
     let mut res = 0;
 
+    let map: Vec<Vec<u8>> = input.lines().map(|l| l.bytes().collect()).collect();
+
+    let mut pos = (0, 0);
+    'outer: for (y, r) in map.iter().enumerate() {
+        for (x, &c) in r.iter().enumerate() {
+            if c == b'S' {
+                pos = (x as i64, y as i64);
+                break 'outer;
+            }
+        }
+    }
+    let mut memo = HashMap::new();
+
+    res += dfs(pos.0, pos.1, &map, &mut memo);
+
     return res;
 }
 
 fn main() {
     let file = file!();
-    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
+    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..])
+        .parse::<i32>()
+        .unwrap_or(0);
 
     println!("Day {}!", nr);
     println!("{}", part1("input".to_string() + &nr.to_string() + ".txt"));
@@ -26,13 +126,17 @@ fn main() {
 #[test]
 fn test_part1() {
     let file = file!();
-    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
-    assert_eq!(0, part1("test".to_string() + &nr.to_string() + ".txt"));
+    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..])
+        .parse::<i32>()
+        .unwrap_or(0);
+    assert_eq!(21, part1("test".to_string() + &nr.to_string() + ".txt"));
 }
 
 #[test]
 fn test_part2() {
     let file = file!();
-    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..]).parse::<i32>().unwrap_or(0);
-    assert_eq!(0, part2("test".to_string() + &nr.to_string() + ".txt"));
+    let nr = (&Path::new(file).file_stem().unwrap().to_str().unwrap()[3..])
+        .parse::<i32>()
+        .unwrap_or(0);
+    assert_eq!(40, part2("test".to_string() + &nr.to_string() + ".txt"));
 }
